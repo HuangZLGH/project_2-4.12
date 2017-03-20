c6db3d3db577b2120672a44f51e54c149ffed83e
zuyi53 <312438600@qq.com>
2017-03-20 20:31:54 +0800

    4
    4

---
+++ Source/project_2/project_2.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "project_2.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, project_2, "project_2" );
